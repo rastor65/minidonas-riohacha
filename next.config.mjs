ef3411cc6f58919135da6d43a9ec6430e9c80742
rastor65 <rdamianquintero@uniguajira.edu.co>
2025-08-14 00:25:00 -0500

    .

--- next.config.mjs
+++ next.config.mjs
@@ -1,18 +1,29 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  images: {
-    unoptimized: true,
-  },
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   eslint: {
+//     ignoreDuringBuilds: true,
+//   },
+//   typescript: {
+//     ignoreBuildErrors: true,
+//   },
+//   images: {
+//     unoptimized: true,
+//   },
 
-  output: 'export',
-  distDir: 'dist',
-}
+//   output: 'export',
+//   distDir: 'dist',
+// }
 
 
-export default nextConfig
+// export default nextConfig
+
+const isProd = process.env.NODE_ENV === 'production';
+const repo = 'minidonas-riohacha'; // nombre exacto de tu repo
+
+export default {
+  output: 'export',              // genera /out automáticamente
+  images: { unoptimized: true }, // evitar optimización en server
+  basePath: isProd ? `/${repo}` : '',
+  assetPrefix: isProd ? `/${repo}/` : '',
+  trailingSlash: true,
+};
